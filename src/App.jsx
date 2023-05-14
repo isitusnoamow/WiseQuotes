395651f0c2b5f518de1bca46b04aca468e062c50
isitusnoamow <240135@hkis.edu.hk>
2023-05-14 22:44:08 +0800

    styling changes

--- src/App.jsx
+++ src/App.jsx
@@ -15,13 +15,13 @@ function App() {
   },[])
   
   const createQuote = async () => {
-    let prompt = "Please give me a short quote that contains two parts inspired by the likes of Sun-Tzu, Buddha, Gandhi and Einstein but not from any of them. Seperate the two parts with a | symbol and only respond with the quote and have no other punctuation. DO not include quotation marks:"
+    let prompt = "Please give me a short (less than 30 words) quote that contains two parts inspired by the likes of Sun-Tzu, Buddha, Gandhi and Einstein but not from any of them. Seperate the two parts with a | symbol and only respond with the quote and have no other punctuation. DO not include quotation marks:"
     try {
       console.log(import.meta.VITE_OPENAI_API_KEY)
       const result = await openai.createCompletion({
         model: "text-davinci-003",
         prompt: prompt,
-        temperature: 0.9,
+        temperature: 0.95,
         max_tokens: 4000,
       });
       //console.log("response", result.data.choices[0].text);
@@ -36,7 +36,7 @@ function App() {
 
   return (
     <>
-      <div id="container" className='text-shadow-solid text-2xl w-fit p-4'>
+      <div id="container" className='text-shadow-solid text-2xl w-fit p-4 font-bold'>
         <h1 className='m-0 text-left'>{apiResponse}</h1>
         <h1 className='m-0 text-right'>{apiResponse2}</h1>
       </div>
